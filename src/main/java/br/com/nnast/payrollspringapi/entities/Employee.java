bcaf83359c522cac5c9a9a9ed88035641febc189
nathalia <ttnast05@gmail.com>
2023-05-07 09:10:51 -0400

    chore: refactoring entities and adding lombok as dependency

--- src/main/java/br/com/nnast/payrollspringapi/entities/Employee.java
+++ src/main/java/br/com/nnast/payrollspringapi/entities/Employee.java
@@ -1,12 +1,16 @@
 package br.com.nnast.payrollspringapi.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Objects;
-
+@Data
 @Entity
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Employee {
     private @Id
     @GeneratedValue Long id;
@@ -14,77 +18,15 @@ public class Employee {
     private String lName;
     private String role;
 
-    public Employee() {
-    }
-
     public Employee(String fName, String lName, String role) {
         this.fName = fName;
         this.lName = lName;
         this.role = role;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return this.fName + " " + this.lName;
-    }
-
     public void setName(String name) {
         String[] parts = name.split(" ");
         this.fName = parts[0];
         this.lName = parts[1];
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Objects.equals(id, employee.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", fName='" + fName + '\'' +
-                ", lName='" + lName + '\'' +
-                ", role='" + role + '\'' +
-                '}';
     }
 }
--- src/main/java/br/com/nnast/payrollspringapi/repositories/OrderRepository.java
+++ src/main/java/br/com/nnast/payrollspringapi/repositories/OrderRepository.java
@@ -2,6 +2,7 @@ package br.com.nnast.payrollspringapi.repositories;
 
 import br.com.nnast.payrollspringapi.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 }
